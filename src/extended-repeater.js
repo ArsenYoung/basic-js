d73c6cfd576107663466ecd8ae55fd02cbae55d9
arsenyoung <molodoiars@gmail.com>
2024-02-14 17:31:56 +0400

    feat: made extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let resultArr = [];
+  let additionArr = [];
+  let sep;
+  let addSep;
+
+  if (!options.separator) {
+    sep = '+';
+  } else {
+    sep = options.separator;
+  }
+
+  if (!options.additionSeparator) {
+    addSep = '|';
+  } else if (options.additionSeparator === null) {
+    addSep = 'null';
+  } else {
+    addSep = options.additionSeparator;
+  }
+
+  if (!Number.isFinite(options.additionRepeatTimes) && !Number.isFinite(options.repeatTimes)) {
+    return str + options.addition;
+  }
+
+  let additionRepeatTimes;
+  if (!options.additionRepeatTimes && addSep) {
+    additionRepeatTimes = 1;
+  } else {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+
+  for (let i = 0; i < additionRepeatTimes; i += 1) {
+    if (options.addition === null) {
+      additionArr.push('null');
+      continue;
+    }
+    additionArr.push(options.addition);
+  }
+
+  let additionFull;
+
+  additionFull = additionArr.join(addSep);
+  
+
+  for (let i = 0; i < options.repeatTimes; i += 1) {
+    resultArr.push(str + additionFull);
+  }
+
+  return resultArr.join(sep);
 }
 
 module.exports = {
