51015a2edba1c7e2f2bb3d0c2db994036f7342db
arsenyoung <molodoiars@gmail.com>
2024-02-14 15:34:54 +0400

    feat: made simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chainValues: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chainValues !== null)
+      return this.chainValues.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    let valueToAdd = '';
+    if (value === null)
+      valueToAdd = 'null';
+    else if (value === undefined) {
+      valueToAdd = 'undefined';
+    } else {
+      valueToAdd = value.toString();
+    }
+    if (this.chainValues !== null) {
+      this.chainValues.push(`( ${valueToAdd} )`);
+    }
+
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
+  removeLink(position) {
+      if (this.chainValues === null) {
+        return this;
+      }
+
+      if (position > this.chainValues.length) {
+        this.chainValues = [];
+        throw new Error("You can\'t remove incorrect link!");
+      }
+
+      if (typeof position !== 'number') {
+        this.chainValues = [];
+        throw new Error("You can\'t remove incorrect link!");
+      }
+
+      if (position <= 0) {
+        this.chainValues = [];
+        throw new Error("You can\'t remove incorrect link!");
+      }
+
+      if (this.chainValues !== null)
+        this.chainValues.splice(position - 1, 1);
+    
+      return this;
+},
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chainValues != null && this.chainValues.length > 1) {
+      this.chainValues.reverse();
+    }
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chainValues === null) {
+      return this;
+    }
+    let result = this.chainValues.join('~~');
+    this.chainValues = [];
+    return result;
   }
 };
 
