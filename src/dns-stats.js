24a5192c88f0b0ded440a33ba37f8b6156893577
arsenyoung <molodoiars@gmail.com>
2024-02-21 16:37:35 +0400

    feat: made dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  
+  for (let item of domains) {
+      let arr = item.split('.');
+      let domain = '';
+      for (let i = arr.length - 1; i >= 0; i--) {
+          domain += `.${arr[i]}`;
+          if (result.hasOwnProperty(domain)) {
+              result[domain]++;
+          } else {
+              result[domain] = 1;
+          }
+      }
+  }
+
+  return result;
 }
 
 module.exports = {
